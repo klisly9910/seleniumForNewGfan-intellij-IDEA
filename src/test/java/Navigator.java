58a679cd7cf2b3d434f432adc1a1791098d44acc
ftangxiaohua@163.com <ftangxiaohua@163.com>
2016-11-11 18:39:00 +0800

    update Navigator.java

--- src/test/java/Navigator.java
+++ src/test/java/Navigator.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
@@ -22,7 +23,8 @@ public class Navigator {
         driver = new FirefoxDriver();
 //        Driver.getDriver("chrome");
 //        driver = new ChromeDriver();
-        driver.get("http://www.gfan.com/new/");
+//        driver.get("http://www.gfan.com/new/");
+        driver.get("http://www.baidu.com");
         driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
@@ -33,24 +35,17 @@ public class Navigator {
         driver.close();
         driver.quit();
     }
-
+    @Test
     public void TestNavigator() throws Exception {
-        String data = FileUtils.readFileToString(new File("mainpage.json"), "UTF-8");
-        JSONObject jsonObject = new JSONObject(data);
-        String phone = jsonObject.getJSONObject("MainPage").getString("phone");
-        String computer = jsonObject.getJSONObject("MainPage").getString("computer");
-        String smart = jsonObject.getJSONObject("MainPage").getString("smart");
-        String homeElectric = jsonObject.getJSONObject("MainPage").getString("homeElectric");
-        String office = jsonObject.getJSONObject("MainPage").getString("office");
-        String game = jsonObject.getJSONObject("MainPage").getString("game");
-        String bbs = jsonObject.getJSONObject("MainPage").getString("bbs");
-        String newMedia = jsonObject.getJSONObject("MainPage").getString("newMedia");
-
-        WebElement ephone = driver.findElement(By.xpath(phone));
-        ephone.click();
-
-
-
+//        String ephone = ConfigData.getInstance().getPhone();
+//        System.out.println("ephone:" + ephone);
+//        WebElement phone = driver.findElement(By.xpath(ephone));
+//        phone.click();
+        String id = ConfigData.getInstance().getId();
+        System.out.println("id = " + id);
+        WebElement ID = driver.findElement(By.id(id));
+        ID.sendKeys("maven");
+        driver.findElement(By.id(ConfigData.getInstance().getButton())).click();
 
 
     }
