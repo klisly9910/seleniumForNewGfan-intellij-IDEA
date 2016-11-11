421f1e6c0959eb5cb7ec84432bf2bee893e517a3
ftangxiaohua@163.com <ftangxiaohua@163.com>
2016-11-11 11:05:17 +0800

    new gfan

---
+++ src/main/java/ParserJson.java
@@ -0,0 +1,18 @@
+
+
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+
+/**
+ * Created by xiaohua on 11/11 0011.
+ */
+public class ParserJson {
+    public void parser() throws Exception {
+        String data = FileUtils.readFileToString(new File("mainpage.json"));
+//        JSONObject jsonObject = new JSONObject(data);
+
+
+    }
+}
